23dfa1a19bb1d605e8d3edc4027061467f121328
BelhassenOunaies <47637036+BelhassenOunaies@users.noreply.github.com>
2019-03-31 13:15:44 +0100

    Add files via upload

---
+++ enigme.h
@@ -0,0 +1,14 @@
+#ifndef ENIGME_H_INCLUDED
+#define ENIGME_H_INCLUDED
+
+
+ int gen_nb(); //genere un nombre aleatoire
+ int gen_en(int k); // genere une enigme aleatoirement
+ void trois_en(int k); // generer 3 enigmes,
+ /* dans le fichier des enigmes, les enigmes seront
+ rangées par 3, cad enigme 1 et 2 et 3 qui seront ligne
+ 1 2 et 3 seront appelées en 1 partie, si on appelle ligne 1
+ on aura ligne 2 et 3, si on appelle ligne 4 on aura ensuite ligne 5 et 6
+ et ainsi de suite .... */
+
+#endif // ENIGME_H_INCLUDED
--- main.c
+++ main.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"SDL/SDL.h"
-#include"SDL/SDL_image.h"
-#include"fonction.h"
-int main(){
-acteur acteur;
-animation(&acteur);
-SDL_FreeSurface(acteur.screen);
-SDL_FreeSurface(acteur.image);
-SDL_Quit();
-return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include  <ctype.h>
+#include <string.h>
+#include "enigme.h"
+
+int main(int argc, char *argv[])
+{
+    int k,j;
+
+trois_en(k);
+    return 0;
 }
